eb02fe9180087bb45e72adaa991e5340c4c7747a
unknown <you@example.com>
2025-07-05 01:03:12 +0300

    qq

--- src/test/java/KYBTestclass.java
+++ src/test/java/KYBTestclass.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class KYBTestclass {
@@ -10,6 +14,10 @@ public class KYBTestclass {
           this.driver = driver;
           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
           driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/main/div/div[1]/div/div[1]/button")).click();
+          WebDriverWait wathaqWait=new WebDriverWait(driver, Duration.ofSeconds(10));
+          WebElement wathaq=wathaqWait.until(ExpectedConditions.presenceOfElementLocated(By.id("registration_id")));
+          wathaq.sendKeys("7001234567");
+          driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div[2]/div/form/button")).click();
 
       }
 }
